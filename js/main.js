7571348652d3f29c28c9451efd38e661d6fe6d43
lulets-l <46016065+lulets@users.noreply.github.com>
2020-02-02 21:49:08 +0400

    added storage of user more preference

--- js/main.js
+++ js/main.js
@@ -14,16 +14,30 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-// Dark / Light Mode switch
+// Dark/Light Mode switch - with user preference storage
 const toggleSwitch = document.querySelector('.mode-switch input[type="checkbox"]');
 
 function switchMode(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   }
   else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
   }
 }
 
 toggleSwitch.addEventListener('change', switchMode, false);
+
+
+// Load users-preference by default if available
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
